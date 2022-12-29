a842e251b960f34206ee54d225b8efaf55c4b89a
明石凜太 <jeffrey.bezos@icloud.com>
2022-12-29 16:02:52 +0900

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Tony Stark on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
