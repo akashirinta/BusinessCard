7bf68a561058f875beca51731bf3f2eb5bfc2447
maple-fm <fuma.deguchi@gmail.com>
2023-01-08 00:51:23 +0900

    保存するデータの追加

--- BusinessCard/CreateModel.swift
+++ BusinessCard/CreateModel.swift
@@ -12,36 +12,23 @@ struct CreateModel {
 
     let user = User(uuid: "qppSOpqb6mc8VeNV6i54", name: "fuma")
 
-//    func getData(card: Card) {
-//        print(card)
-//        let cardRef = user.getAuth().collection("cards").document("ksIi0cte3GbBHufctWk8")
-//        cardRef.getDocument { (user, error) in
-//            if let user = user {
-//                let data = user.data().map(String.init(describing: )) ?? "nil"
-//                print("Document data: \(data)")
-//            } else {
-//                print("Document does not exist")
-//            }
-//        }
-//
-//    }
-
     func saveData(card: Card) {
-        var ref: DocumentReference? = nil
-        let cardRef = user.getAuth().collection("cards").addDocument(data: [
+        user.getAuth().collection("cards").addDocument(data: [
             "name": card.name,
             "furigana": card.furigana,
-            "organizationName": card.organizationName
+            "organizationName": card.organizationName,
+            "mailaddress": card.mailaddress ?? "",
+            "phoneNumber": card.phoneNumber ?? "",
+            "address": card.address ?? "",
+            "twitter": card.twitter ?? "",
+            "instagram": card.Instagram ?? ""
         ]) { err in
             if let err = err {
                 print("Error adding document: \(err)")
-            } else {
-                print("Document added with ID: \(ref!.documentID)")
             }
 
         }
     }
-
 
 
     func isValidated(name: String?, furigana: String?, organizationName: String?) -> Bool {
