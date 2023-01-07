dca213367bdcc0ad0ede8812fa256ac747b72df5
maple-fm <fuma.deguchi@gmail.com>
2023-01-08 00:50:28 +0900

    名刺の構造体を修正

--- BusinessCard/Model/Card.swift
+++ BusinessCard/Model/Card.swift
@@ -9,7 +9,6 @@ import Foundation
 
 struct Card: Codable {
 
-    var uuid: String
     var name: String = ""
     var furigana: String = ""
     var organizationName: String = ""
@@ -21,14 +20,13 @@ struct Card: Codable {
 
     init(name: String, furigana: String, organizationName: String, mailaddress: String?, phoneNumber: String?, address: String?, twitter: String?, Instagram: String?) {
 
-        self.uuid = UUID().uuidString
         self.name = name
         self.furigana = furigana
         self.organizationName = organizationName
         self.mailaddress = mailaddress
         self.phoneNumber = phoneNumber
         self.address = address
-        self.twitter = twitter
-        self.Instagram = Instagram
+        self.twitter = "https://twitter.com/\(String(describing: twitter))/"
+        self.Instagram = "https://www.instagram.com/\(String(describing: Instagram))/"
     }
 }
